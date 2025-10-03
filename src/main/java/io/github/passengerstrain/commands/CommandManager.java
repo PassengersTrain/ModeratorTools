ec312be7cd876f8cb0f5ce984c9bda8222a307c8
PassengersTrain <proxydoesmc@gmail.com>
2025-10-03 09:54:17 +0530

    Update CommandManager.java
    Recoded certain classes related with configuration

--- src/main/java/io/github/passengerstrain/commands/CommandManager.java
+++ src/main/java/io/github/passengerstrain/commands/CommandManager.java
@@ -2,7 +2,8 @@ package io.github.passengerstrain.commands;
 
 import io.github.passengerstrain.commands.subcommands.FreezePlayerCommand;
 import io.github.passengerstrain.commands.subcommands.UnfreezePlayerCommand;
-import io.github.passengerstrain.utils.Utils;
+import io.github.passengerstrain.utils.Config;
+import lombok.NonNull;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,47 +12,46 @@ import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 
-
 public class CommandManager implements CommandExecutor {
 
-    private Utils utils;
+    private final Config config;
+    private final ArrayList<SubCommand> subcommands = new ArrayList<>();
 
-    private ArrayList<SubCommand> subcommands = new ArrayList<>();
+    public CommandManager(@NonNull Config config) {
+        this.config = config;
 
-    public CommandManager(Utils utils) {
-        this.utils = utils;
-    }
-
-    public CommandManager(){
-        subcommands.add(new FreezePlayerCommand(utils));
-        subcommands.add(new UnfreezePlayerCommand(utils));
+        subcommands.add(new FreezePlayerCommand(config));
+        subcommands.add(new UnfreezePlayerCommand(config));
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+    public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command, @NonNull String label, @NonNull String[] args) {
         if (sender instanceof Player){
             Player player = (Player) sender;
 
             if (args.length > 0){
+
                 for (int i = 0; i < getSubcommands().size(); i++){
                     if (args[0].equalsIgnoreCase(getSubcommands().get(i).getName())){
                         getSubcommands().get(i).executeCommand(player, args);
                     }
                 }
-            } else if(args.length == 0){
+            } else {
                 for (int i = 0; i < getSubcommands().size(); i++){
-                  final String insufficientCommandArgs = utils.getLanguageConfig().getString("language.insufficientCommandArgs");
+                  final String insufficientCommandArgs = config.getInsufficientCommandArgs();
+                    assert insufficientCommandArgs != null;
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', insufficientCommandArgs));
                 }
             }
-
+        } else {
+            final String nonConsoleCommand = config.getNotPlayerCommandExecutor();
+            if (nonConsoleCommand != null) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', nonConsoleCommand));
+            }
         }
         return true;
     }
-
     public ArrayList<SubCommand> getSubcommands(){
         return subcommands;
     }
-
 }
